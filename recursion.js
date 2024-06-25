d2bc3c0ac51a36073c50f202e4f63a452ec10810
Clay O'Dell <clay.odell.music@gmail.com>
2024-06-25 16:00:43 -0500

    added longest, everyOther, and isPalindrome

--- recursion.js
+++ recursion.js
@@ -5,3 +5,26 @@ function product(arr) {
     return arr[0] * product(arr.slice(1));
   }
 }
+function longest(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  const maxLen = arr[0].length;
+  const restArr = arr.slice(1);
+  return Math.max(maxLen, longest(restArr));
+}
+function everyOther(str) {
+    if (str === '') {
+        return '';
+    }
+    return str[0] + everyOther(str.slice(2));
+}
+function isPalindrome(str) {
+    if (str.length <= 1) {
+        return true;
+    }
+    if (str[0] !== str[str.length - 1]) {
+        return false;
+    }
+    return isPalindrome(str.slice(1, str.length -1));
+}
